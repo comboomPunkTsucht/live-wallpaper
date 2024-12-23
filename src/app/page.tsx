adc0bf069e38c4772cd9c25a558c43a6d2b9d360
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2024-12-23 03:31:15 +0100

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ function GetH2() {
 
   return myH2 === 'comboom.sucht' ? (
     <Image
-      src="./Logo-transparet.PNG"
+      src="/Logo-transparet.PNG"
       alt="Logo"
       width={300}
       height={180}
