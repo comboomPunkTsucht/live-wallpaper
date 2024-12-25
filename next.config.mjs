fc1c269900398a1c48cff6c71ecec5f7ed1a0c89
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2024-12-25 07:18:09 +0100

    remove unused development setup for Cloudflare integration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,3 @@
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: false,
@@ -41,9 +40,5 @@ const nextConfig = {
     ],
   },
 };
-
-if (process.env.NODE_ENV === 'development') {
-   await setupDevPlatform();
- }
 
 export default nextConfig;
