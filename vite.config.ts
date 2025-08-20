d83a76c6b8ca36b8ffe5afad7719b8eef86ec819
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2025-08-20 20:51:20 +0200

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
 import { defineConfig } from 'vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import { resolve } from 'node:path'
+import viteTsConfigPaths from 'vite-tsconfig-paths'
+import { tanstackRouter } from '@tanstack/router'
+import { resolve, __dirname } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    TanStackRouterVite({ autoCodeSplitting: true }),
+    viteTsConfigPaths({
+      projects: ['./tsconfig.json'],
+    }),
+    tanstackRouter(),
     viteReact(),
     tailwindcss(),
   ],
