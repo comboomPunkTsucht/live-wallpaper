4f513f9918e12b1e26b1df8d2606a3772f2e10fb
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2025-08-20 21:07:06 +0200

    .

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,5 +17,8 @@ export default defineConfig({
     }),
     viteReact(),
     tailwindcss(),
+    cloudflare(
+      { viteEnvironment:{name: "ssr"}, persistState: true, },
+    ),
   ],
 })
